5046d5eb8127811eaced5308ea06d4ba092ed5f2
Pagan Gazzard <page@balena.io>
2019-06-17 16:59:40 +0100

    Change `IsDistinctFrom` for postgres to a version that can use indexes
    Change-type: patch

--- test/abstract-sql/is-distinct.ts
+++ test/abstract-sql/is-distinct.ts
@@ -1,28 +1,91 @@
 import { AbstractSqlQuery } from '../../src/AbstractSQLCompiler';
 
+type TestCb = (
+	result: { query: string },
+	sqlEquals: (a: string, b: string) => void,
+) => void;
 // tslint:disable-next-line no-var-requires
 const test = require('./test') as (
 	query: AbstractSqlQuery,
-	cb: (
-		result: { query: string },
-		sqlEquals: (a: string, b: string) => void,
-	) => void,
+	binds: any[][] | TestCb,
+	cb?: TestCb,
 ) => void;
 
-test(
-	['SelectQuery', ['Select', [['IsDistinctFrom', ['Number', 1], ['Null']]]]],
-	(result, sqlEquals) => {
-		it('should produce a valid is distinct from statement', () => {
-			sqlEquals(result.query, 'SELECT 1 IS DISTINCT FROM NULL');
-		});
-	},
-);
+describe('IsDistinctFrom', () => {
+	test(
+		[
+			'SelectQuery',
+			['Select', [['IsDistinctFrom', ['Number', 1], ['Number', 2]]]],
+		],
+		(result, sqlEquals) => {
+			it('should produce a valid is distinct from statement', () => {
+				sqlEquals(
+					result.query,
+					'SELECT NOT((1) IS NOT NULL AND (2) IS NOT NULL AND (1) = (2) OR (1) IS NULL AND (2) IS NULL)',
+				);
+			});
+		},
+	);
+	test(
+		[
+			'SelectQuery',
+			['Select', [['IsDistinctFrom', ['Number', 1], ['Text', '2']]]],
+		],
+		[['Text', '2']],
+		(result, sqlEquals) => {
+			it('should produce a valid is distinct from statement', () => {
+				sqlEquals(result.query, 'SELECT NOT((1) IS NOT NULL AND (1) = ($1))');
+			});
+		},
+	);
 
-test(
-	['SelectQuery', ['Select', [['IsNotDistinctFrom', ['Number', 1], ['Null']]]]],
-	(result, sqlEquals) => {
-		it('should produce a valid is not distinct from statement', () => {
-			sqlEquals(result.query, 'SELECT 1 IS NOT DISTINCT FROM NULL');
-		});
-	},
-);
+	test(
+		['SelectQuery', ['Select', [['IsDistinctFrom', ['Number', 1], ['Null']]]]],
+		(result, sqlEquals) => {
+			it('should produce an is not null statement', () => {
+				sqlEquals(result.query, 'SELECT 1 IS NOT NULL');
+			});
+		},
+	);
+});
+
+describe('IsNotDistinctFrom', () => {
+	test(
+		[
+			'SelectQuery',
+			['Select', [['IsNotDistinctFrom', ['Number', 1], ['Number', 2]]]],
+		],
+		(result, sqlEquals) => {
+			it('should produce a valid is not distinct from statement', () => {
+				sqlEquals(
+					result.query,
+					'SELECT (1) IS NOT NULL AND (2) IS NOT NULL AND (1) = (2) OR (1) IS NULL AND (2) IS NULL',
+				);
+			});
+		},
+	);
+	test(
+		[
+			'SelectQuery',
+			['Select', [['IsNotDistinctFrom', ['Number', 1], ['Text', '2']]]],
+		],
+		[['Text', '2']],
+		(result, sqlEquals) => {
+			it('should produce a valid is not distinct from statement', () => {
+				sqlEquals(result.query, 'SELECT (1) IS NOT NULL AND (1) = ($1)');
+			});
+		},
+	);
+
+	test(
+		[
+			'SelectQuery',
+			['Select', [['IsNotDistinctFrom', ['Number', 1], ['Null']]]],
+		],
+		(result, sqlEquals) => {
+			it('should produce a valid is not distinct from statement', () => {
+				sqlEquals(result.query, 'SELECT 1 IS NULL');
+			});
+		},
+	);
+});
