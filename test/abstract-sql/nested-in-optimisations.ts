e7010af9667c48db526d65d1286dd3c88f2bf831
Pagan Gazzard <page@balena.io>
2019-10-11 18:13:21 +0100

    Allow combining/adding to existing NOT IN statements
    Change-type: patch

--- test/abstract-sql/nested-in-optimisations.ts
+++ test/abstract-sql/nested-in-optimisations.ts
@@ -141,3 +141,51 @@ describe('OR IN/EQUALs should create a single IN statement', () => {
 		},
 	);
 });
+
+describe('AND NOT IN/NOT EQUALs should create a single NOT IN statement', () => {
+	test(
+		[
+			'SelectQuery',
+			['Select', []],
+			['From', ['Table', 'table']],
+			[
+				'Where',
+				[
+					'And',
+					[
+						'NotIn',
+						['ReferencedField', 'table', 'field1'],
+						['Text', 'a'],
+						['Text', 'b'],
+					],
+					[
+						'And',
+						[
+							'NotEquals',
+							['ReferencedField', 'table', 'field1'],
+							['Text', 'c'],
+						],
+						[
+							'NotEquals',
+							['ReferencedField', 'table', 'field1'],
+							['Text', 'd'],
+						],
+					],
+				],
+			],
+		],
+		[['Text', 'a'], ['Text', 'b'], ['Text', 'c'], ['Text', 'd']],
+		(result, sqlEquals) => {
+			it('should produce a single not in statement', () => {
+				sqlEquals(
+					result.query,
+					stripIndent`
+						SELECT 1
+						FROM "table"
+						WHERE "table"."field1" NOT IN ($1, $2, $3, $4)
+					`,
+				);
+			});
+		},
+	);
+});
