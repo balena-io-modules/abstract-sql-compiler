214523e0cfe05e256c2d1c67c48231968a32d620
Pagan Gazzard <page@balena.io>
2025-06-20 15:40:35 +0100

    Switch to a separate `viewDefinition` property for creating views
    This removes the inference of whether to create a view from based on
    inspecting the definition and instead makes it a conscious choice to
    avoid converting a definition to a view in cases where that may not
    make sense, eg if using a definition over a materialized table in order
    to have a dynamic view over that table
    
    Change-type: major

--- src/AbstractSQLCompiler.ts
+++ src/AbstractSQLCompiler.ts
@@ -494,6 +494,7 @@ export interface AbstractSqlTable {
 	primitive: false | string;
 	triggers?: Trigger[];
 	checks?: Check[];
+	viewDefinition?: Omit<Definition, 'binds'>;
 	definition?: Definition;
 	modifyFields?: AbstractSqlTable['fields'];
 	modifyName?: AbstractSqlTable['name'];
@@ -815,21 +816,24 @@ $$;`);
 		if (typeof table === 'string') {
 			return;
 		}
-		const { definition } = table;
-		if (definition != null) {
+		const { definition, viewDefinition } = table;
+		if (viewDefinition != null && definition != null) {
+			throw new Error('Cannot have both a definition and a viewDefinition');
+		}
+		if (definition != null || viewDefinition != null) {
 			if (table.fields.some(({ computed }) => computed != null)) {
 				throw new Error(
 					`Using computed fields alongside a custom table definition is unsupported, found for table resourceName: '${table.resourceName}', name: '${table.name}'`,
 				);
 			}
-			if (definition.binds != null && definition.binds.length > 0) {
-				// If there are any binds then it's a dynamic definition and cannot become a view
-				return;
-			}
-			let definitionAbstractSql = definition.abstractSql;
+		}
+		if (viewDefinition != null) {
+			let definitionAbstractSql = viewDefinition.abstractSql;
 			// If there are any resource nodes then it's a dynamic definition and cannot become a view
 			if (abstractSqlContainsNode(definitionAbstractSql, isResourceNode)) {
-				return;
+				throw new Error(
+					`Cannot create a view from a dynamic viewDefinition, found for table resourceName: '${table.resourceName}', name: '${table.name}'`,
+				);
 			}
 			if (isTableNode(definitionAbstractSql)) {
 				// If the definition is a table node we need to wrap it in a select query for the view creation
