b54ec3a27411b354e192238d4af0797e2becf976
Thodoris Greasidis <thgreasi@gmail.com>
2026-03-11 17:01:05 +0200

    Split the check-constraint schema optimization to a separate file
    Change-type: patch
    See: https://balena.fibery.io/Work/Project/961

---
+++ src/schema-optimizations/check-constraint.ts
@@ -0,0 +1,94 @@
+import type {
+	AbstractSqlModel,
+	AbstractSqlQuery,
+	AbstractSqlType,
+	BooleanTypeNodes,
+	Check,
+	WhereNode,
+} from '../abstract-sql-compiler.js';
+import { isFromNode, isSelectQueryNode } from '../abstract-sql-compiler.js';
+import { generateRuleSlug } from '../abstract-sql-schema-optimizer.js';
+
+const countFroms = (n: AbstractSqlType[]) => {
+	let count = 0;
+	n.forEach((p) => {
+		if (Array.isArray(p)) {
+			if (isFromNode(p)) {
+				count++;
+			} else {
+				count += countFroms(p as AbstractSqlType[]);
+			}
+		}
+	});
+	return count;
+};
+
+const convertReferencedFieldsToFields = (nodes: AbstractSqlType[]) => {
+	for (let i = 0; i < nodes.length; i++) {
+		const node = nodes[i];
+		if (Array.isArray(node)) {
+			if (node[0] === 'ReferencedField') {
+				nodes[i] = ['Field', node[2]];
+			} else {
+				convertReferencedFieldsToFields(node as AbstractSqlType[]);
+			}
+		}
+	}
+};
+
+export function convertRuleToCheckConstraint(
+	abstractSqlModel: AbstractSqlModel,
+	ruleSE: string,
+	ruleBody: AbstractSqlQuery,
+) {
+	const count = countFroms(ruleBody);
+
+	if (
+		count === 1 &&
+		(ruleBody[0] === 'NotExists' ||
+			(ruleBody[0] === 'Equals' &&
+				ruleBody[2][0] === 'Number' &&
+				ruleBody[2][1] === 0)) &&
+		isSelectQueryNode(ruleBody[1])
+	) {
+		const selectQueryNodes = ruleBody[1].slice(1);
+		if (
+			selectQueryNodes.every((n) => ['Select', 'From', 'Where'].includes(n[0]))
+		) {
+			let fromNode = selectQueryNodes.find(isFromNode)![1];
+			if (fromNode[0] === 'Alias') {
+				fromNode = fromNode[1];
+			}
+			if (fromNode[0] === 'Table') {
+				const whereNodes = selectQueryNodes.filter(
+					(n): n is WhereNode => n[0] === 'Where',
+				);
+				let whereNode: BooleanTypeNodes;
+				if (whereNodes.length > 1) {
+					whereNode = ['And', ...whereNodes.map((n) => n[1])];
+				} else {
+					whereNode = whereNodes[0][1];
+				}
+				// This replaces the `Not` we stripped from the `NotExists`
+				whereNode = ['Not', whereNode];
+
+				convertReferencedFieldsToFields(whereNode);
+
+				const tableName = fromNode[1];
+				const table = Object.values(abstractSqlModel.tables).find(
+					(t) => t.name === tableName,
+				);
+				if (table) {
+					table.checks ??= [];
+					const check: Check = {
+						description: ruleSE,
+						name: generateRuleSlug(tableName, ruleBody),
+						abstractSql: whereNode,
+					};
+					table.checks.push(check);
+					return check;
+				}
+			}
+		}
+	}
+}
